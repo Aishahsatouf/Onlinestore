47adabd41a2ed607f5af5ee791d2dd0a30e999d9
Aishahsatouf <aishasattouf1996@gmail.com>
2020-09-22 18:05:36 +0300

    addjs

---
+++ js/js.js
@@ -0,0 +1,14 @@
+var sam = 'sara';
+var sam = prompt('please enter your name')
+
+var number = '10';
+var number = prompt('please enter your age')
+
+var day = new Date();
+var hournow = prompt ('please write the date today')
+if (hournow< 15) {
+    alert ('good day')
+} else {
+    alert ('wish you a good day')
+}
+ document.write(greeting);
