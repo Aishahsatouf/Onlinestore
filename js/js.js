3a267e180c75de791f31f68aaacfe1620ab6db61
Aishahsatouf <aishasattouf1996@gmail.com>
2020-09-23 11:52:51 +0300

    adding function

--- js/js.js
+++ js/js.js
@@ -1,14 +1,15 @@
-var sam = 'sara';
-var sam = prompt('please enter your name')
 
-var number = '10';
-var number = prompt('please enter your age')
 
-var day = new Date();
-var hournow = prompt ('please write the date today')
-if (hournow< 15) {
-    alert ('good day')
-} else {
-    alert ('wish you a good day')
+function greeting(){
+    
+    var day = new Date();
+    var hournow = prompt ('please write the date today')
+    if (hournow< 15) {
+        greeting= 'Welcome to our store';
+    } else {
+        greeting= 'Hello this is my store';
+    }
+     document.write(greeting);
 }
- document.write(greeting);
+
+greeting();
